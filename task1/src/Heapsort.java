c23576f348dc44ab532908157c60ed2e4b1dfb1e
miika <miika.karjalainen13@gmail.com>
2023-11-29 19:52:44 +0200

    Implement heapsort

--- task1/src/Heapsort.java
+++ task1/src/Heapsort.java
@@ -8,9 +8,49 @@ public class Heapsort {
     }
 
     public void heapsort() {
+        // Build max heap
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            heapify(arr, arr.length, i);
+        }
 
+        // One by one extract elements from heap
+        for (int i = arr.length - 1; i > 0; i--) {
+            // Swap the root (maximum element) with the last element
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
 
+            // Call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
     }
+
+    private void heapify(int[] arr, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int leftChild = 2 * i + 1; // left = 2*i + 1
+        int rightChild = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (leftChild < n && arr[leftChild] > arr[largest]) {
+            largest = leftChild;
+        }
+
+        // If right child is larger than largest so far
+        if (rightChild < n && arr[rightChild] > arr[largest]) {
+            largest = rightChild;
+        }
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
 
     public void printarr() {
         for(int i = 0; i < arr.length; i++) {
--- task1/src/Main.java
+++ task1/src/Main.java
@@ -1,23 +1,49 @@
+import java.util.Calendar;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        int[] rand_arr = gen_arr(1000);
+        int[] rand_arr2 = gen_arr(10000);
+        int[] rand_arr3 = gen_arr(100000);
+        int[] rand_arr4 = gen_arr(1000000);
+        //int[] rand_arr5 = gen_arr(10000000);
+
+        test_bubblesort(rand_arr);
+
+
+        test_heapsort(rand_arr4);
+
+    }
+    private static void test_bubblesort(int[] rand_arr) {
+        Calendar time1 = Calendar.getInstance();
+        long start1 = time1.getTimeInMillis();
+        Bubblesort bubblesort = new Bubblesort(rand_arr);
+        bubblesort.bubblesort();
+        Calendar time2 = Calendar.getInstance();
+        long end1 = time2.getTimeInMillis();
+        System.out.println("Time taken for array size of " + rand_arr.length + ": " + (end1 - start1) + "ms");
+    }
+
+    private static void test_heapsort(int[] rand_arr) {
+        Calendar time3 = Calendar.getInstance();
+        long start2 = time3.getTimeInMillis();
+        Heapsort heapsort = new Heapsort(rand_arr);
+        heapsort.heapsort();
+        Calendar time4 = Calendar.getInstance();
+        long end2 = time4.getTimeInMillis();
+        System.out.println("Time taken for array size of " + rand_arr.length + ": " + (end2 - start2) + "ms");
+    }
+
+    private static int[] gen_arr(int size) {
         Random random = new Random();
 
-        int[] rand_arr = new int[1000];
+        int[] rand_arr = new int[size];
 
-        for(int i = 0; i < 1000; i++) {
+        for(int i = 0; i < size; i++) {
             rand_arr[i] = random.nextInt(3000000);
         }
 
-
-        Bubblesort bubblesort = new Bubblesort(rand_arr);
-        bubblesort.bubblesort();
-        bubblesort.printarr();
-
-        Heapsort heapsort = new Heapsort(rand_arr);
-        heapsort.heapsort();
-
-
+        return rand_arr;
     }
 }
